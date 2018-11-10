383a1b80463958e765b1fee3de603d8ed74bb6b3
Raphael Novello <33816373+RaphaelAN@users.noreply.github.com>
2018-11-10 20:17:15 -0200

    Inicio do projeto
    Editei o programa para ler o arquivo Ur_Tabuleiro.txt e printar o tabuleiro uma vez. Essa adição prova o conceito que usaremos para trabalhar com o tabuleiro criado em ascii.

--- ur.c
+++ ur.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
-int main(){
-  
+#include <stdlib.h>
+
+
+int main()
+{
+  FILE* tabuleiro;
+  int c;
+  if((tabuleiro = fopen("Ur_Tabuleiro.txt","r")) == NULL)
+  {
+    printf("Não foi possivel inicializar o tabuleiro\n");
+    return 1;
+  }
+  printf("\n");
+  while(1)
+  {
+    c = fgetc(tabuleiro);
+    if (feof(tabuleiro))
+    {
+      break;
+    }
+    printf("%c", c);
+  }
+
   return 0;
 }
